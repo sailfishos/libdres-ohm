f73c2bc5a0d12e1c57ae3bb92fb26ca754815627
Krisztian Litkey <kli@kliptop.localdomain>
2008-04-30 16:19:59 +0300

    Separated (fact) variables to factvar.c

--- src/dres.c
+++ src/dres.c
@@ -13,7 +13,6 @@
 extern FILE *yyin;
 extern int   yyparse(dres_t *dres);
 
-static void free_variables(dres_t *dres);
 static void free_literals(dres_t *dres);
 static int  finalize_variables(dres_t *dres);
 static int  finalize_actions(dres_t *dres);
@@ -77,7 +76,7 @@ dres_exit(dres_t *dres)
         return;
     
     dres_free_targets(dres);
-    free_variables(dres);
+    dres_free_variables(dres);
     free_literals(dres);
     
     dres_store_destroy(dres->fact_store);
@@ -183,49 +182,6 @@ free_prereq(dres_prereq_t *dep)
 }
 
 
-/*****************************************************************************
- *                          *** variable handling ***                        *
- *****************************************************************************/
-
-/********************
- * dres_add_variable
- ********************/
-int
-dres_add_variable(dres_t *dres, char *name)
-{
-    dres_variable_t *var;
-    int              id;
-
-    if (!REALLOC_ARR(dres->variables, dres->nvariable, dres->nvariable + 1))
-        return DRES_ID_NONE;
-
-    id  = dres->nvariable++;
-    var = dres->variables + id;
-
-    var->id   = DRES_VARIABLE(id);
-    var->name = STRDUP(name);
-
-    return var->name ? var->id : DRES_ID_NONE;
-}
-
-
-/********************
- * dres_variable_id
- ********************/
-int
-dres_variable_id(dres_t *dres, char *name)
-{
-    dres_variable_t *var;
-    int              i;
-
-    if (name != NULL)
-        for (i = 0, var = dres->variables; i < dres->nvariable; i++, var++) {
-            if (!strcmp(name, var->name))
-                return var->id;
-        }
-    
-    return dres_add_variable(dres, name);
-}
 
 
 /********************
@@ -265,30 +221,6 @@ finalize_actions(dres_t *dres)
 
     DRES_SET_FLAG(dres, ACTIONS_FINALIZED);
     return 0;
-}
-
-
-/********************
- * free_variables
- ********************/
-static void
-free_variables(dres_t *dres)
-{
-    int              i;
-    dres_variable_t *var;
-
-    for (i = 0, var = dres->variables; i < dres->nvariable; i++, var++) {
-        FREE(var->name);
-#if 0
-        if (var->var)
-            dres_var_destroy(var->var);
-#endif
-    }
-    
-    FREE(dres->variables);
-
-    dres->variables = NULL;
-    dres->nvariable = 0;
 }
 
 
@@ -884,27 +816,6 @@ dres_update_goal(dres_t *dres, char *goal)
     dres_free_graph(graph);
     
     return EINVAL;
-}
-
-
-/********************
- * dres_check_variable
- ********************/
-int
-dres_check_variable(dres_t *dres, int id, int refstamp)
-{
-    dres_variable_t *var = dres->variables + DRES_INDEX(id);
-    char             buf[32];
-    
-    DEBUG("%s: %d > %d ?", dres_name(dres, id, buf, sizeof(buf)),
-          var->stamp, refstamp);
-    
-#ifdef STAMP_FORCED_UPDATE
-    var->stamp = dres->stamp+1;          /* fake that variables have changed */
-    return TRUE;
-#endif
-    
-    return var->stamp > refstamp;
 }
 
 
