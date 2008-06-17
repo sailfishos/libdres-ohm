d57987c7c68e099eb10288cf926938592f543de7
Krisztian Litkey <kli@kliptop.localdomain>
2008-06-17 13:54:57 +0300

    Limit the visibility of symbols that are not part of the public library API.

---
+++ acinclude.m4
@@ -0,0 +1,19 @@
+AC_DEFUN([AC_CHECK_CC_VISIBILITY],
+    [AC_CACHE_CHECK([whether ${CC-cc} supports -fvisibility=hidden],
+         [ac_cv_cc_visibility],
+         [echo 'void f(){}' > conftest.c
+          if test -z "`${CC-cc} -c -fvisibility=hidden conftest.c 2>&1`"; then
+            ac_cv_cc_visibility=yes
+          else
+            ac_cv_cc_visibility=no
+          fi])
+     if test $ac_cv_cc_visibility = yes; then
+       AC_DEFINE([HAVE_VISIBILITY_SUPPORT], 1,
+                 [Define to 1 if your compiler supports -fvisibility=hidden.])
+       CCOPT_VISIBILITY_HIDDEN="-fvisibility=hidden"
+     else
+       AC_DEFINE([HAVE_VISIBILITY_SUPPORT], [],
+                 [Define to 1 if your compiler supports -fvisibility=hidden.])
+       CCOPT_VISIBILITY_HIDDEN=""
+     fi
+    ])
---
+++ include/dres/compiler.h
@@ -0,0 +1,81 @@
+#ifndef __DRES_COMPILER_H__
+#define __DRES_COMPILER_H__
+
+/*
+ * Notes:
+ *
+ *   This file defines compiler-specific macros for a few commonly used
+ *   things that are not in the scope of the C99 and are thus not standardized.
+ *   Currently these are
+ *
+ *   1) Visibility constraints: macros for controlling the scope of visibility
+ *      of symbols with non-static scope
+ *
+ *      It is often comes handy, especially for libraries, to declare variables
+ *      and functions with non-static scope (apart from the public API provided
+ *      by the library) so that they can be accessed from all the modules of
+ *      the library. However, these symbols should not appear in the global
+ *      symbol table as they are not part of the public API and are not
+ *      supposed to be accessed outside the library.
+ *
+ *      Here is the typical way of using these macros:
+ *
+ *        - Do not export global symbols by default. This is accomplished
+ *          either by putting the NOEXPORT_BY_DEFAULT in the beginning
+ *          of every module or a commonly included header or by instructing your
+ *          compiler/linker to do so (eg. with -fvisibility=hidden for GCC)
+ *        - marking symbols to be exported by the EXPORTED macro
+ *
+ *      Depending on the ratio of exported vs. all non-static variables it
+ *      might make more sense to do it the other way around, ie. export by
+ *      default and mark the ones you do not want to expose with NOEXPORT.
+ *
+ *
+ *   2) Branch-prediction hints: macros for asking the compiler to generate
+ *      code with controlled branch-prediction characteristics.
+ *
+ *      You tell the compiler which side of the branch you want to optimize for
+ *      by telling whether the branch condition is more likely to be true or
+ *      false. These macros are very similar to the ones used by the linux
+ *      kernel.
+ */
+
+
+#include <dres/config.h>                   /* HAVE_VISIBILITY_SUPPORT */
+
+
+#if __GNUC__
+
+#  if HAVE_VISIBILITY_SUPPORT
+#    define NOEXPORT_BY_DEFAULT _Pragma("GCC visibility push(hidden)")
+#    define EXPORTED_BY_DEFAULT _Pragma("GCC visibility push(default)")
+
+#    define NOEXPORT __attribute__ ((visibility("hidden")))
+#    define EXPORTED __attribute__ ((visibility("default")))
+#  endif /* HAVE_VISIBILITY_SUPPORT */
+
+#  ifndef unlikely
+#    define unlikely(cond) __builtin_expect(cond, 0)
+#  endif
+#  ifndef likely
+#    define likely(cond) __builtin_expect(cond, 1)
+#  endif
+
+#else /* !__GNUC__ */
+
+#  warning "Compiler specific macros are not defined for your compiler."
+
+#  warning "Visibility constraint macros not defined for your compiler."
+#  define NOEXPORT_BY_DEFAULT
+#  define EXPORTED_BY_DEFAULT
+#  define NOEXPORT
+#  define EXPORTED
+
+#  warning "Branch-prediction hint macros not defined for your compiler."
+#  define unlikely(cond) (cond)
+#  define likely(cond)   (cond)
+
+#endif /* !__GNUC__ */
+
+
+#endif /* __DRES_COMPILER_H__ */
