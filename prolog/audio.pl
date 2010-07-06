eb0ca8b705e3331563aa7c6fa398f8fa05226fba
Wolf Bergenheim <ext-wolf.2.bergenheim@nokia.com>
2010-07-06 09:42:21 +0300

    Updated license header to be < 78 characters wide.

--- prolog/audio.pl
+++ prolog/audio.pl
@@ -1,3 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file is part of dres the resource policy dependency resolver.
+% 
+% Copyright (C) 2010 Nokia Corporation.
+% 
+% This library is free software; you can redistribute
+% it and/or modify it under the terms of the GNU Lesser General Public
+% License as published by the Free Software Foundation
+% version 2.1 of the License.
+% 
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+% Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Lesser General Public
+% License along with this library; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+% USA.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 :- module(audio,
 	  [set_routes/1, set_volume_limits/1, set_corks/1, playback_request/3,
 	  available_accessory/2, privacy_override/1, active_policy_group/1,
